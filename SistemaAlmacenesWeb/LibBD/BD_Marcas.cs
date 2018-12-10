79566462a46d8d6b4238ef1147c56b5809420e77
riosvjose <ji.rios@acad.ucb.edu.bo>
2018-12-10 17:12:48 -0400

    BD_Marcas

---
+++ SistemaAlmacenesWeb/LibBD/BD_Marcas.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using nsGEN_OracleBD;
+using nsGEN_Mensajes;
+using nsGEN_Cadenas;
+using System.Collections;
+
+namespace SistemaAlmacenesWeb
+{
+    // Creado por: Ignacio Rios; Fecha: 10/12/2018
+    // Ultima modificación: Ignacio Rios; Fecha: 10/12/2018
+    // Descripción: Clase referente a la tabla alm_marcas
+    public class BD_Marcas
+    {
+        #region Variables Locales
+        GEN_OracleBD OracleBD = new GEN_OracleBD();
+        GEN_Mensajes libMensajes = new GEN_Mensajes();
+        GEN_Cadenas libCadenas = new GEN_Cadenas();
+        private string strSql = string.Empty;
+        #endregion
+
+        #region Atributos
+        // Campos de la tabla alm_marcas
+        private long _num_sec_marca = 0;
+        private string _nombre = string.Empty;
+        private string _fecharegistro = string.Empty;
+        private string _usuarioregistro = string.Empty;
+        private long _numsecusuarioregistro = 0;
+
+        // Otras propiedades
+        private string _mensaje = string.Empty;
+        private string _strconexion = string.Empty;
+
+
+        public long NumSecMarca
+        {
+            get { return _num_sec_marca; }
+            set { _num_sec_marca = value; }
+        }
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        public string FechaRegistro
+        {
+            get { return _fecharegistro; }
+        }
+        public string UsuarioRegistro
+        {
+            get { return _usuarioregistro; }
+        }
+        public long NumSecUsuarioRegistro
+        {
+            get { return _numsecusuarioregistro; }
+            set { _numsecusuarioregistro = value; }
+        }
+
+        public string Mensaje
+        {
+            get { return _mensaje; }
+        }
+        public string StrConexion
+        {
+            get { return _strconexion; }
+            set { _strconexion = value; }
+        }
+        #endregion
+
+        #region Constructores
+        public BD_Marcas()
+        { 
+            _num_sec_marca = 0;
+            _nombre = string.Empty;
+            _fecharegistro = string.Empty;
+            _usuarioregistro = string.Empty;
+            _numsecusuarioregistro = 0;
+
+            _mensaje = string.Empty;
+            _strconexion = string.Empty;
+        }
+        #endregion
+
+        #region Metodos
+        public bool Insertar(bool blAsignarFuncionesPerfil, long lngNSPerfil, bool blObligarCambioPWD)
+        {
+            bool blOperacionCorrecta = false;
+            strSql = "insert into alm_marcas (num_sec_usuario, num_sec_persona, usuario, login, activo, fecha_vigencia, num_sec_usuario_reg) values";
+            strSql += " (alm_marcas_sec.nextval";
+           /* strSql += " , " + _numsecpersona.ToString();
+            strSql += " , '" + _usuario.Trim() + "'";
+            strSql += " , '" + _login.Trim().ToUpper() + "'";
+            strSql += " , " + _activo.ToString();
+            strSql += " , to_date('" + _fechavigencia.Trim() + "','dd/mm/rrrr')";*/
+            strSql += " , " + _numsecusuarioregistro.ToString();
+            strSql += " )";
+            OracleBD.MostrarError = false;
+            OracleBD.StrConexion = _strconexion;
+            OracleBD.Sql = strSql;
+            OracleBD.EjecutarSqlTrans();
+            _mensaje = OracleBD.Mensaje;
+            blOperacionCorrecta = !OracleBD.Error;
+            if (OracleBD.Error)
+                _mensaje = "No fue posible insertar el dato. Se encontró un error al insertar en la tabla alm_marcas. " + _mensaje;
+            return blOperacionCorrecta;
+        }
+
+        public bool Modificar()
+        {
+            bool blOperacionCorrecta = false;
+            if (blOperacionCorrecta)
+            {
+                strSql = "update alm_marcas set ";
+                strSql += " nombre = " + _nombre;
+                strSql += " where num_sec_marca = " + _num_sec_marca.ToString();
+
+                OracleBD.MostrarError = false;
+                OracleBD.StrConexion = _strconexion;
+                OracleBD.Sql = strSql;
+                OracleBD.EjecutarSqlTrans();
+
+                _mensaje = OracleBD.Mensaje;
+                blOperacionCorrecta = !OracleBD.Error;
+                if (OracleBD.Error)
+                    _mensaje = "No fue posible actualizar el dato. Se encontró un error al actualizar en la tabla alm_marcas. " + _mensaje;
+            }
+            return blOperacionCorrecta;
+        }
+
+        public bool Borrar()
+        {
+            bool blOperacionCorrecta = false;
+            strSql = "delete alm_marcas ";
+            strSql += " where num_sec_marca = " + _num_sec_marca.ToString();
+
+            OracleBD.MostrarError = false;
+            OracleBD.StrConexion = _strconexion;
+            OracleBD.Sql = strSql;
+            OracleBD.EjecutarSqlTrans();
+
+            _mensaje = OracleBD.Mensaje;
+            blOperacionCorrecta = !OracleBD.Error;
+            if (OracleBD.Error)
+                _mensaje = "No fue posible borrar el dato. Se encontró un error al eliminar en la tabla alm_marcas. " + _mensaje;
+            return blOperacionCorrecta;
+        }
+
+        public bool Ver(string strNSUsuario)
+        {
+            bool blEncontrado = false;
+            string strSql = string.Empty;
+            if (!string.IsNullOrEmpty(strNSUsuario))
+            {
+                strSql = "select a.num_sec_usuario, a.num_sec_persona, a.usuario, a.login, a.activo";
+
+                DataTable dt = new DataTable();
+                OracleBD.MostrarError = false;
+                OracleBD.StrConexion = _strconexion;
+                OracleBD.Sql = strSql;
+                OracleBD.sqlDataTable();
+                dt = OracleBD.DataTable;
+                if (dt.Rows.Count > 0)
+                {
+                    blEncontrado = true;
+                    DataRow dr = dt.Rows[0];
+                    _num_sec_marca = Convert.ToInt64(dr["num_sec_usuario"].ToString());
+                    _nombre = dr["usuario"].ToString();
+                    _fecharegistro = dr["f_reg"].ToString();
+                    _usuarioregistro = dr["u_reg"].ToString();
+                    _numsecusuarioregistro = Convert.ToInt64(dr["num_sec_usuario_reg"].ToString());
+                }
+                dt.Dispose();
+            }
+            if (!blEncontrado)
+            {
+                _num_sec_marca = 0;
+                _nombre = string.Empty;
+                _fecharegistro = string.Empty;
+                _usuarioregistro = string.Empty;
+                _numsecusuarioregistro = 0;
+
+                _mensaje = string.Empty;
+                _strconexion = string.Empty;
+            }
+            return blEncontrado;
+        }
+
+        #endregion
+
+        #region Procedimientos y Funciones Locales
+        
+        #endregion
+
+    }
+
+
+}
