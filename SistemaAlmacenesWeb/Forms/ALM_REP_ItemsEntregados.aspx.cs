1b762a46a5aac9cbefa2a36212b48b64832085f9
alvaroml <alvaro.mamani.limachi@gmail.com>
2019-01-10 09:51:33 -0400

    Permiso de reportes

--- SistemaAlmacenesWeb/Forms/ALM_REP_ItemsEntregados.aspx.cs
+++ SistemaAlmacenesWeb/Forms/ALM_REP_ItemsEntregados.aspx.cs
@@ -20,6 +20,7 @@ namespace SistemaAlmacenesWeb.Forms
         GEN_Java libJava = new GEN_Java();
         GEN_WebForms webForms = new GEN_WebForms();
         SIS_GeneralesSistema Generales = new SIS_GeneralesSistema();
+        BD_ProcAdicionales libproc = new BD_ProcAdicionales();
         #endregion
 
         #region "Clase de tablas de la Base de Datos"
@@ -31,12 +32,21 @@ namespace SistemaAlmacenesWeb.Forms
         {
             if (!string.IsNullOrEmpty(strCon.Trim()))
             {
-                // Recibir mensaje exitoso cuando se redirige de otra pagina
-                if (Session["MensajeOK"] != null)
+                libproc.StrConexion = axVarSes.Lee<string>("strConexion");
+                if (libproc.AccesoObjetoUsuario("ALM_REP_ItemsEntregados"))
                 {
-                    pnMensajeOK.Visible = true;
-                    lblMensajeOK.Text = Session["MensajeOK"].ToString();
-                    Session["MensajeOK"] = null;
+                    // Recibir mensaje exitoso cuando se redirige de otra pagina
+                    if (Session["MensajeOK"] != null)
+                    {
+                        pnMensajeOK.Visible = true;
+                        lblMensajeOK.Text = Session["MensajeOK"].ToString();
+                        Session["MensajeOK"] = null;
+                    }
+                }
+                else
+                {
+                    axVarSes.Escribe("MostrarMensajeError", "1");
+                    Response.Redirect("Index.aspx");
                 }
             }
             else
