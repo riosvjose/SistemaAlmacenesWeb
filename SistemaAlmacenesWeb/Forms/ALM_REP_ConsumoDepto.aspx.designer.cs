ce162f4ae563a844d91ecf9df1bc78fc7e33c811
riosvjose <ji.rios@acad.ucb.edu.bo>
2019-01-25 08:44:31 -0400

    Exporta a excel consumo depto

--- SistemaAlmacenesWeb/Forms/ALM_REP_ConsumoDepto.aspx.designer.cs
+++ SistemaAlmacenesWeb/Forms/ALM_REP_ConsumoDepto.aspx.designer.cs
@@ -139,6 +139,15 @@ namespace SistemaAlmacenesWeb.Forms {
         protected global::System.Web.UI.WebControls.Label lblRepConsumoDepto;
         
         /// <summary>
+        /// ibtnExportarExcel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ibtnExportarExcel;
+        
+        /// <summary>
         /// gvConsumoDepto control.
         /// </summary>
         /// <remarks>
